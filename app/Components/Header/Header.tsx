b134e3a141607d2208c596ac9f699ddce0dda1c2
Laksh <laksh.prabhudessai@gmail.com>
2023-12-18 17:36:23 +0530

    Initial changes

---
+++ app/Components/Header/Header.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import cartIcon from "../../assets/cart.svg";
+
+const Header = () => {
+  return (
+    <>
+      {/* {cartIcon} */}
+      <div className="flex justify-between">
+        <div>
+          <text className="font-medium">E-Commerce</text>
+        </div>
+        <div className="w-8 h-8">
+          <img src={cartIcon?.src ?? ""} alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
